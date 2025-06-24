23f1050e85d7e669549ed7e71571a16a8b24c929
David <dromang5@gmail.com>
2025-06-24 12:37:26 +0200

    first commit other local

---
+++ tests/testBookin.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from "@playwright/test";
+
+
+test("Serch home in a Coruna", async ({ page }) => {
+  await page.goto("https://www.booking.com");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(
+    "Booking.com | Official site | The best hotels, flights, car rentals & accommodations");
+  // Fill in the destination and press Enter 
+  await page.locator('input[name="ss"]').fill('A Coruña');
+  await page.waitForTimeout(2000); // autosuggest
+  const pressEnter = await page.keyboard.press('Enter');
+  // pressEnter
+  // console.log(pressEnter)
+
+});
+
+//prueba de commit
